f11e9514b6b2cd8926411a473e92d7c08dd972de
chandupoduli007 <chandupoduli007@gmail.com>
2025-04-10 15:17:05 -0400

    QuizApp

---
+++ src/test/java/com/telusko/Quizapp/QuizappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telusko.Quizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
